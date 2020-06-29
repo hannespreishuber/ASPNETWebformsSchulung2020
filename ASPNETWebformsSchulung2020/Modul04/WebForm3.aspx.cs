6a80da1a3f528dc233dcc9b2d34b520f4f8bddce
Hannes Preishuber <hannesp@ppedv.de>
2020-06-29 19:14:52 +0200

    Mail Versand

--- ASPNETWebformsSchulung2020/Modul04/WebForm3.aspx.cs
+++ ASPNETWebformsSchulung2020/Modul04/WebForm3.aspx.cs
@@ -10,6 +10,7 @@ namespace ASPNETWebformsSchulung2020.Modul04
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
+                
         protected void Page_Load(object sender, EventArgs e)
         {
 
