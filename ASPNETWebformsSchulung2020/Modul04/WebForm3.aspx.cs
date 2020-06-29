790f29a6830057a79a0e23ed8b7978fb4bd23050
Hannes Preishuber <hannesp@ppedv.de>
2020-06-29 16:52:14 +0200

    Email Verschicken per SMTP Service

---
+++ ASPNETWebformsSchulung2020/Modul04/WebForm3.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul04
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var sc = new SmtpClient();
+            sc.Send("hannesp@ppedv.de", "billg@microsoft.com", "will dein Geld", "Textinhalt");
+        }
+    }
+}
