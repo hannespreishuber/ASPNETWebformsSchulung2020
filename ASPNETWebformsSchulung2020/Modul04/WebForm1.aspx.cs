4c6531adfe11c76c023d0d3573d6241878c4a558
Hannes Preishuber <hannesp@ppedv.de>
2020-06-29 16:26:33 +0200

    Dynamische Buttons mit Eventhandler

---
+++ ASPNETWebformsSchulung2020/Modul04/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul04
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var btn = new Button();
+                btn.Text = "Text" + i.ToString();
+                btn.CommandArgument = i.ToString();
+                btn.Click += new EventHandler(Button1_Click);
+                form1.Controls.Add(btn);
+
+            }
+
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = ((Button)sender).CommandArgument;
+        }
+    }
+}
