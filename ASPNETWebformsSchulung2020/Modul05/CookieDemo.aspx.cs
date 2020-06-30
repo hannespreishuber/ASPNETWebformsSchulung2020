897e377973fdd9a48a05b8f2aeaec480bd74ac01
Hannes Preishuber <hannesp@ppedv.de>
2020-06-30 11:33:52 +0200

    Session s und Cookies

---
+++ ASPNETWebformsSchulung2020/Modul05/CookieDemo.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul05
+{
+    public partial class CookieDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var mycookie = new HttpCookie("ppedvCookie");
+            mycookie.Value = "Hannes Preishuber";
+            mycookie.Expires = DateTime.Now.AddDays(30);
+            mycookie.Domain = "localhost";
+
+            Response.Cookies.Add(mycookie);
+
+          
+        }
+    }
+}
---
+++ ASPNETWebformsSchulung2020/Modul05/SessionDemo.aspx.cs
@@ -0,0 +1,36 @@
+﻿using ASPNETWebformsSchulung2020.Modul04;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul05
+{
+    public partial class SessionDemo : System.Web.UI.Page
+    {
+        public List<String> ToDoItems { get; set; } = new List<string>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (  Session["todos"]==null)
+            {
+                Session.Add("todos",ToDoItems);
+            }
+
+            ToDoItems = (List<String>)Session["todos"];
+    
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+          
+            ToDoItems.Add(task.Text);
+            task.Text = "";
+            Session["todos"] = ToDoItems;
+
+        }
+    }
+}
