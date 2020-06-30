53cb47a5625a47d2642289f0ea2479541ecb2bcc
Hannes Preishuber <hannesp@ppedv.de>
2020-06-30 10:17:51 +0200

    HTTP Helper in Klassen

---
+++ ASPNETWebformsSchulung2020/Modul05/DBFake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETWebformsSchulung2020.Modul05
+{
+    public class DBFake
+    {
+        public DBFake()
+        {
+            var x=HttpUtility.HtmlEncode("üöß");
+        }
+    }
+}
