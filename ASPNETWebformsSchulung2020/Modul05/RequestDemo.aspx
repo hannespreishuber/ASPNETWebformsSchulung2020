7e8afbbf04f3cffdbeaffba7b470b20000cfc936
Hannes Preishuber <hannesp@ppedv.de>
2020-06-30 10:13:13 +0200

    Request Demo Querystring, Form, Friendlyurl

---
+++ ASPNETWebformsSchulung2020/Modul05/RequestDemo.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RequestDemo.aspx.cs" Inherits="ASPNETWebformsSchulung2020.Modul05.RequestDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" method="post">
+        <div>
+            <input name="TextBox1" />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASPNETWebformsSchulung2020/Modul05/RequestDemo.aspx.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.FriendlyUrls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul05
+{
+    public partial class RequestDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var wert = Request.Form["TextBox1"];
+            var wert1 = Request.QueryString["TextBox1"];
+            var wert2 = Request.GetFriendlyUrlSegments();
+            var wert4 = Request.Browser;
+            var wert5 = Request.Params;
+          
+        }
+    }
+}
