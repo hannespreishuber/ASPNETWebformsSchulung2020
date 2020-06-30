0dd483e8b4aba0e8b304bacd11c6ae4999de08d8
Hannes Preishuber <hannesp@ppedv.de>
2020-06-30 09:16:55 +0200

    Page Lifecycle

---
+++ ASPNETWebformsSchulung2020/Modul05/MyPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace ASPNETWebformsSchulung2020.Modul05
+{
+    public class MyPage : Page
+    {
+        protected override void OnInit(EventArgs e)
+        {
+           
+        }
+    }
+}
