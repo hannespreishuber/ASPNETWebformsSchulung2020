a4cf983706f73be125aaf1c09f6834b184ae605c
Hannes Preishuber <hannesp@ppedv.de>
2020-06-30 09:49:25 +0200

    REesponse Objekt

---
+++ ASPNETWebformsSchulung2020/Modul05/ResponseDemo.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul05
+{
+    public partial class ResponseDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Clear(); 
+            Response.StatusCode = 404;// todo
+            Response.Redirect("Redirect.aspx");
+            
+        }
+    }
+}
---
+++ ASPNETWebformsSchulung2020/Modul05/redirect.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul05
+{
+    public partial class redirect : System.Web.UI.Page
+    {
+        public int Count { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Count++;
+            Response.Write("xxxxxxxxxxxxxxxx");
+        }
+    }
+}
