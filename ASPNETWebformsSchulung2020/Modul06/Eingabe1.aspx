1862611d30b1e282d4aa60e8893dcd1f87e80f33
Hannes Preishuber <hannesp@ppedv.de>
2020-06-30 13:50:56 +0200

    Bootstrap Formualre mit Checkbox

---
+++ ASPNETWebformsSchulung2020/Modul06/Eingabe1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Eingabe1.aspx.cs" Inherits="ASPNETWebformsSchulung2020.Modul06.Eingabe1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+        <div class="col">
+
+            <div class="form-group row">
+                <label for="txtEmail" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+
+                    <asp:TextBox ID="txtEmail" runat="server" class="form-control"></asp:TextBox>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="txtName" class="col-sm-2 col-form-label">Fam. Name</label>
+                <div class="col-sm-10">
+
+                    <asp:TextBox ID="txtName" runat="server" class="form-control"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col">
+
+                    <div class="custom-control custom-checkbox">
+                      
+                        <asp:CheckBox ID="customCheck1" runat="server" 
+                            ClientIDMode="Static" 
+                            />
+                        <label class="custom-control-label" for="customCheck1">Check </label>
+                    </div>
+                </div>
+                <div class="col-sm-10">
+
+                    <asp:Button ID="Button1" runat="server" Text="Save" class="btn btn-outline-primary"
+                        OnClick="Button1_Click" />
+
+                </div>
+            </div>
+        </div>
+    </div>
+      <script>
+       $(function () {
+        $('input:checkbox').addClass('custom-control-input');
+     });
+    </script>
+</asp:Content>
---
+++ ASPNETWebformsSchulung2020/Modul06/Eingabe1.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace ASPNETWebformsSchulung2020.Modul06
+{
+
+
+    public partial class Eingabe1
+    {
+
+        /// <summary>
+        /// txtEmail-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// txtName-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+
+        /// <summary>
+        /// customCheck1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox customCheck1;
+
+        /// <summary>
+        /// Button1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
