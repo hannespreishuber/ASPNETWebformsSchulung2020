6924fe0e346f7c58e4d86319a79859e11b98a8c5
Hannes Preishuber <hannesp@ppedv.de>
2020-07-01 09:33:24 +0200

    SQLDataSource

--- ASPNETWebformsSchulung2020/Modul08/DatenSucheSqlCommand.aspx.cs
+++ ASPNETWebformsSchulung2020/Modul08/DatenSucheSqlCommand.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -16,7 +17,19 @@ namespace ASPNETWebformsSchulung2020.Modul08
             using (var con = new SqlConnection(
                 ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString))
             {
-
+                var cmd = new SqlCommand("SELECT * FROM [Customers] WHERE ([CompanyName] LIKE '%' + @CompanyName + '%')",con);
+                cmd.Parameters.AddWithValue("CompanyName", search.Text);
+                con.Open();
+                //var reader = cmd.ExecuteReader();
+                //while (reader.Read())
+                //{
+                //    Response.Write(reader["CompanyName"]+"<br/>");
+                //}
+                var da = new SqlDataAdapter(cmd);
+                var dt = new DataTable();
+                da.Fill(dt);
+                Repeater1.DataSource = dt;
+                Repeater1.DataBind();
             }
 
         }
