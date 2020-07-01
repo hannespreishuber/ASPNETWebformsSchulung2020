8ea94a5476d4e44287351cfa984275e835323a85
Hannes Preishuber <hannesp@ppedv.de>
2020-07-01 14:44:05 +0200

    Entity Framework Model Binding

--- ASPNETWebformsSchulung2020/Modul09/ToDoPage.aspx.cs
+++ ASPNETWebformsSchulung2020/Modul09/ToDoPage.aspx.cs
@@ -14,6 +14,7 @@ namespace ASPNETWebformsSchulung2020.Modul09
     {
          protected void Page_Load(object sender, EventArgs e)
         {
+
     
 
         }
@@ -21,8 +22,17 @@ namespace ASPNETWebformsSchulung2020.Modul09
         protected void Button1_Click(object sender, EventArgs e)
         {
             var rand = new Random();
-           
 
+            var Aufgabe = new MyToDos()
+            {
+                Bezeichnung = TextBox1.Text,
+                Datum = DateTime.Now.AddSeconds(rand.Next(31536000))
+
+            };
+            var db = new DbTodo();
+            db.MyToDos.Add(Aufgabe);
+            db.SaveChanges();
+            DataBind();
         }
 
         protected void TodoListView_ItemCommand(object source, ListViewCommandEventArgs e)
@@ -32,6 +42,11 @@ namespace ASPNETWebformsSchulung2020.Modul09
                 case "del":
                     var id = e.CommandArgument;
                     //sql delete 
+
+                    var db = new DbTodo();
+
+                    db.MyToDos.Remove(db.MyToDos.Find(id));
+                    db.SaveChanges();
                     break;
 
                 default:
@@ -69,3 +84,6 @@ namespace ASPNETWebformsSchulung2020.Modul09
         }
     }
 }
+//erst EF Model erstellen
+// aspx Seiten Itemtype, Typed Binings #item., SelectMethod generieren
+//INstaz ef, reückgabe todos
