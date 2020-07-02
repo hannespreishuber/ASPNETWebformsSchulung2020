f5b61e1f7f0f81b0bed4c6d7e47ceaf89998ba7e
Hannes Preishuber <hannesp@ppedv.de>
2020-07-02 15:16:41 +0200

    Bildergallerie Responsive

---
+++ ASPNETWebformsSchulung2020/Modul16/BilderGallerie.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BilderGallerie.aspx.cs" Inherits="ASPNETWebformsSchulung2020.Modul16.BilderGallerie" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+    <asp:Repeater ID="Repeater1" runat="server" ItemType="System.String"
+         SelectMethod="Repeater1_GetData"
+        >
+        <ItemTemplate>
+            <div class="col-md-4 col-12">
+            <img src="bilder/<%# Item %>" class="img-fluid"/>
+
+            </div>
+        </ItemTemplate>
+
+    </asp:Repeater>
+</div>
+</asp:Content>
---
+++ ASPNETWebformsSchulung2020/Modul16/BilderGallerie.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETWebformsSchulung2020.Modul16
+{
+    public partial class BilderGallerie : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+                   }
+
+        public IEnumerable<System.String> Repeater1_GetData()
+        {
+            var bilder = Directory.GetFiles(Server.MapPath("~/modul16/bilder"));
+
+            for (int i = 0; i < bilder.Length; i++)
+            {
+                bilder[i] = Path.GetFileName(bilder[i]);
+            }
+            return bilder;
+        }
+    }
+}
