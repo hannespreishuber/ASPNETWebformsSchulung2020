7e530bee935be9f5e2abcd6c86284a5572a31d56
Hannes Preishuber <hannesp@ppedv.de>
2020-07-02 13:55:34 +0200

    SignalR

---
+++ ASPNETWebformsSchulung2020/Modul15/ppedvHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace ASPNETWebformsSchulung2020.Modul15
+{
+    public class ppedvHub : Hub
+    {
+        public void Hello()
+        {
+           
+            Clients.All.hello();
+        }
+        public void SendMsg(string msg,string sender)
+        {
+
+            Clients.All.NeueMsg(msg, sender);
+        }
+    }
+}
