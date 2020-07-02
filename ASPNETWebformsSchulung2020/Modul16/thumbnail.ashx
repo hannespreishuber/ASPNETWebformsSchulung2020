b911f645bcc982dbb2c8d97f109a97f50cc64415
Hannes Preishuber <hannesp@ppedv.de>
2020-07-02 15:32:43 +0200

    Thumbnail Gallery

---
+++ ASPNETWebformsSchulung2020/Modul16/thumbnail.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="thumbnail.ashx.cs" Class="ASPNETWebformsSchulung2020.Modul16.thumbnail" %>
---
+++ ASPNETWebformsSchulung2020/Modul16/thumbnail.ashx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETWebformsSchulung2020.Modul16
+{
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für thumbnail
+    /// </summary>
+    public class thumbnail : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var bild = context.Request.QueryString[0];
+            var img = new Bitmap(context.Server.MapPath("~/modul16/bilder/") + bild);
+            var thumb = img.GetThumbnailImage(600, 400, null, IntPtr.Zero);
+            img.Dispose();
+            var stream = new MemoryStream();
+            thumb.Save(stream, ImageFormat.Jpeg);
+            var buffer = stream.ToArray();
+
+
+            context.Response.ContentType = "image/jpg";
+            context.Response.OutputStream.Write(buffer,0,buffer.Length);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
