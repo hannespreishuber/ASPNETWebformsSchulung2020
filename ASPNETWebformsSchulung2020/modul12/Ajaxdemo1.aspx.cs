0cf8651c3527f1a0fa2e5157a9430b06de5f6517
Hannes Preishuber <hannesp@ppedv.de>
2020-07-02 09:33:45 +0200

    Updatepanel Ajax

--- ASPNETWebformsSchulung2020/modul12/Ajaxdemo1.aspx.cs
+++ ASPNETWebformsSchulung2020/modul12/Ajaxdemo1.aspx.cs
@@ -12,7 +12,7 @@ namespace ASPNETWebformsSchulung2020.modul12
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+ 
         }
 
         protected void Button1_Click(object sender, EventArgs e)
